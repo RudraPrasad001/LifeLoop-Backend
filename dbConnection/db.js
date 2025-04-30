14219eec433fc3ae86375cd2cf5a3b98253a614b
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-04-30 21:52:34 +0530

    Followers System

--- dbConnection/db.js
+++ dbConnection/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import expressAsyncHandler from "express-async-handler";
-const mongose =expressAsyncHandler( async()=>mongoose.connect("mongodb://localhost:27017/")
+const mongose =expressAsyncHandler( async()=>mongoose.connect(process.env.CONNECTION_STRING)
 .then(async ()=>{console.log("db connected");})
 .catch((e)=>{console.log("Error catched "+ e);}));
 export default mongose;
