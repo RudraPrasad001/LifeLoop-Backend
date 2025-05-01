77c9e764074ea9dc124ce20e7bb5963d987bed3e
RudraPrasad001 <rudraprasad.email@gmail.com>
2025-05-01 15:20:05 +0530

    Followers System

--- middlewares/jwtAuthentication.js
+++ middlewares/jwtAuthentication.js
@@ -5,7 +5,7 @@ const jwtAuth = (req,res,next)=>{
     let token = req.cookies?.token;
     if (!token) {
         console.log("No token found, redirecting...");
-        return res.redirect("http://localhost:5173/login");
+        return res.redirect(`${process.env.URL}/login`);
       }
         let user = jwt.verify(token,process.env.SECRET_KEY);
         req.token = user;
@@ -17,7 +17,7 @@ const jwtAuth = (req,res,next)=>{
     catch(e){
         console.log(e);
         res.clearCookie("token");
-        return res.redirect("http://localhost:5173/login");
+        return res.redirect(`${process.env.URL}/login`);
     }
 }
 
